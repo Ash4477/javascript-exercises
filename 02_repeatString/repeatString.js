28a768aa400922c12112a2350f1763571e89821b
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-03-12 12:12:22 +0500

    do exc2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
 
+    new_str = '';
+    for (let i = 0; i < num; i++) {
+        new_str += str;
+    }
+
+    return new_str;
 };
 
 // Do not edit below this line
