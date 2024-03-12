d2cc14876516b8a6bc0c668bf266fd1e8b00e091
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-03-12 12:20:57 +0500

    do exc3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let revWord = "";
 
+    for (let i = 0; i < word.length(); i++) {
+        revWord += word.charAt(i);
+    }
+
+    return revWord;
 };
 
 // Do not edit below this line
