17a8fff3260c236377eae643f681d58178759a83
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-03-12 13:21:34 +0500

    do exc4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...vals) {
 
-};
+    let newArray = [];
+
+    array.forEach(element => {
+        if (!vals.includes(element)) newArray.push(element);
+    });
+
+    return newArray;
+};  
 
 // Do not edit below this line
 module.exports = removeFromArray;
