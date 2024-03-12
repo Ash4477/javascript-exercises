1b1a5cca43e96ae388c6cdbc8534f3f8ec1dbc1c
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-03-12 13:32:24 +0500

    do exc5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
 
+    // if any arg is negative
+    if ((start < 0) || (end < 0) || (typeof start != "number") || (typeof end != "number")) {
+        return 'ERROR';
+    }
+
+    // if start is greater than end
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let finalSum  = 0;
+
+    for (let i = start; i <= end; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
